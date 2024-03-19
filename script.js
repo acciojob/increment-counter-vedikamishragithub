64429d690ea65b4318f99093861c2f2be143c8cb
Vedika Mishra <151787416+vedikamishragithub@users.noreply.github.com>
2024-03-19 13:46:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+var para=document.getElementById("counter");
+
+var text=para.innerText;
+var num=Number(text);
+function onFunction() {	 
+   num++;
+   document.getElementById("counter").innerText=num;
+	
+}
